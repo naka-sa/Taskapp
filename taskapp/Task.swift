f7726d866230bfcc9dbe5bfc198c8919a02c9792
satoshi_nakajima <satoshi_nakajima@softbank-no-macbook.local>
2017-02-28 22:15:53 +0900

    Task.swift追加

---
+++ taskapp/Task.swift
@@ -0,0 +1,30 @@
+//
+//  Task.swift
+//  taskapp
+//
+//  Created by satoshi_nakajima on 2017/02/28.
+//  Copyright © 2017年 satoshi_nakajima. All rights reserved.
+//
+
+import RealmSwift
+
+class Task: Object {
+    // 管理用 ID。プライマリーキー
+    dynamic var id = 0
+    
+    // タイトル
+    dynamic var title = ""
+    
+    // 内容
+    dynamic var contents = ""
+    
+    /// 日時
+    dynamic var date = NSDate()
+    
+    /**
+     id をプライマリーキーとして設定
+     */
+    override static func primaryKey() -> String? {
+        return "id"
+    }
+}
