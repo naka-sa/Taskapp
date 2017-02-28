530464ef7398f418f9a8de0364bbf73adfcb83e3
satoshi_nakajima <satoshi_nakajima@softbank-no-macbook.local>
2017-02-28 20:56:24 +0900

    Initial Commit

---
+++ taskapp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  taskapp
+//
+//  Created by satoshi_nakajima on 2017/02/28.
+//  Copyright © 2017年 satoshi_nakajima. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
