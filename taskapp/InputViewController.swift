0135ab94eb605f068ec8161a0a2646db72c48852
satoshi.nakajima <astoshi.nakajima@g.softbank.co.jp>
2017-03-04 20:34:12 +0900

    ローカル通知の前まで

--- taskapp/InputViewController.swift
+++ taskapp/InputViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import RealmSwift    // 追加する
 
 class InputViewController: UIViewController {
 
@@ -17,17 +18,39 @@ class InputViewController: UIViewController {
         
     @IBOutlet weak var datePicker: UIDatePicker!
     
+    var task: Task!   // 追加する
+    let realm = try! Realm()    // 追加する
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        // 背景をタップしたらdismissKeyboardメソッドを呼ぶように設定する
+        let tapGesture: UITapGestureRecognizer = UITapGestureRecognizer(target:self, action:#selector(dismissKeyboard))
+        self.view.addGestureRecognizer(tapGesture)
+        
+        titleTextField.text = task.title
+        contentsTextView.text = task.contents
+        datePicker.date = task.date as Date
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
+    override func viewWillDisappear(_ animated: Bool) {
+        try! realm.write {
+            self.task.title = self.titleTextField.text!
+            self.task.contents = self.contentsTextView.text
+            self.task.date = self.datePicker.date as NSDate
+            self.realm.add(self.task, update: true)
+        }
+        
+        super.viewWillDisappear(animated)
+    }
+    func dismissKeyboard(){
+        // キーボードを閉じる
+        view.endEditing(true)
+    }
 
     /*
     // MARK: - Navigation
